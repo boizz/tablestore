411aad329e70b8cca7695c44e6125c302a16afcc
Heleth <bon.hoo@hotmail.com>
2019-08-10 22:04:40 +0800

    feat: Batch return transform

--- test/__data__/utils.js
+++ test/__data__/utils.js
@@ -2,7 +2,7 @@ const TableStore = require('tablestore');
 
 const { Long } = TableStore;
 
-exports.RETURN = {
+exports.RETURN_MULTIPLE = {
   simple: {
     rows: [
       {
@@ -58,5 +58,124 @@ exports.RETURN = {
         col5: 123456789,
       },
     ],
+  },
+};
+
+exports.RETURN_SINGLE = {
+  simple: {
+    row: {
+      primaryKey: [
+        {
+          name: 'gid',
+          value: Long.fromNumber(20013),
+        },
+        {
+          name: 'uid',
+          value: Long.fromNumber(20018),
+        },
+      ],
+      attributes: [
+        {
+          columnName: 'col1',
+          columnValue: '表格存储',
+          timestamp: 1564290238349,
+        },
+        {
+          columnName: 'col2',
+          columnValue: '2',
+          timestamp: 1564290238349,
+        },
+        {
+          columnName: 'col3',
+          columnValue: 3.1,
+          timestamp: 1564290238349,
+        },
+        {
+          columnName: 'col4',
+          columnValue: -0.32,
+          timestamp: 1564290238349,
+        },
+        {
+          columnName: 'col5',
+          columnValue: Long.fromNumber(123456789),
+          timestamp: 1564290238349,
+        },
+      ],
+    },
+  },
+  target: {
+    data: {
+      gid: 20013,
+      uid: 20018,
+      col1: '表格存储',
+      col2: '2',
+      col3: 3.1,
+      col4: -0.32,
+      col5: 123456789,
+    },
+  },
+};
+
+exports.RETURN_TABLES = {
+  simple: {
+    tables: [
+      [
+        {
+          tableName: 'sampleTable',
+          primaryKey: [
+            {
+              name: 'gid',
+              value: Long.fromNumber(20013),
+            },
+            {
+              name: 'uid',
+              value: Long.fromNumber(20018),
+            },
+          ],
+          attributes: [
+            {
+              columnName: 'col1',
+              columnValue: '表格存储',
+              timestamp: 1564290238349,
+            },
+            {
+              columnName: 'col2',
+              columnValue: '2',
+              timestamp: 1564290238349,
+            },
+            {
+              columnName: 'col3',
+              columnValue: 3.1,
+              timestamp: 1564290238349,
+            },
+            {
+              columnName: 'col4',
+              columnValue: -0.32,
+              timestamp: 1564290238349,
+            },
+            {
+              columnName: 'col5',
+              columnValue: Long.fromNumber(123456789),
+              timestamp: 1564290238349,
+            },
+          ],
+        },
+      ],
+    ],
+  },
+  target: {
+    data: {
+      sampleTable: [
+        {
+          gid: 20013,
+          uid: 20018,
+          col1: '表格存储',
+          col2: '2',
+          col3: 3.1,
+          col4: -0.32,
+          col5: 123456789,
+        },
+      ],
+    },
   },
 };
--- test/utils/dataTransform.test.js
+++ test/utils/dataTransform.test.js
@@ -2,11 +2,17 @@ const assert = require('power-assert');
 
 const { TRANSFORM_RETURN } = require('../../lib/utils/dataTransform');
 const { compare } = require('../__utils__/data');
-const { RETURN } = require('../__data__/utils');
+const { RETURN_SINGLE, RETURN_MULTIPLE, RETURN_TABLES } = require('../__data__/utils');
 
 
 describe('数据处理', () => {
-  it('输出数据转换', () => {
-    assert(compare(RETURN.target.data, TRANSFORM_RETURN(RETURN.simple).data));
+  it('单行数据输出', () => {
+    assert(compare(RETURN_SINGLE.target.data, TRANSFORM_RETURN(RETURN_SINGLE.simple).data));
+  });
+  it('多行数据输出', () => {
+    assert(compare(RETURN_MULTIPLE.target.data, TRANSFORM_RETURN(RETURN_MULTIPLE.simple).data));
+  });
+  it('多表数据输出', () => {
+    assert(compare(RETURN_TABLES.target.data, TRANSFORM_RETURN(RETURN_TABLES.simple).data));
   });
 });
